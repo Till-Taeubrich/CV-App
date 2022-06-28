73756ba2cd1bafff38aab5490bcabf5b49a4c1ed
Till Taeubrich <Till.taeubrich@gmail.com>
2022-06-28 10:42:15 +1000

    add Inputblocks

--- src/components/CVEdit.js
+++ src/components/CVEdit.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BlockTitle from "./BlockTitle";
 import InputField from "./InputField";
+import InputBlock from './InputBlock';
 
 class CVEdit extends React.Component {
     render() {
@@ -14,7 +15,9 @@ class CVEdit extends React.Component {
                 <InputField/>
                 <InputField/>
                 <BlockTitle content='Education' />
+                <InputBlock/>
                 <BlockTitle content='Work Experience' />
+                <InputBlock/>
             </form>
         )
     }
